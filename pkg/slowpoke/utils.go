0b4a6350b751002a6e4a64da7188d2055cb8ea04
yizhengx <yizhengx@node0.slowpoke.brown-atlas-pg0.utah.cloudlab.us>
2025-02-21 00:17:13 -0700

    Add counting requests

--- pkg/slowpoke/utils.go
+++ pkg/slowpoke/utils.go
@@ -12,41 +12,86 @@ import (
 	"runtime"
 	"github.com/eniac/mucache/pkg/common"
 	"github.com/eniac/mucache/pkg/utility"
+	"sync"
 )
 
 const printIntervalMillis = 30*1000
 // {"thread_id" :{"requestName": counter}}
 var (
-	requestCounters map[int]map[string]int
+	// requestCounters map[int]map[string]int
 	delayMicros int
+	prerun bool
+	requestCounters sync.Map
 )
 
-func printCounters() {
-	time_now := time.Now()
-	func_counters := make(map[string]int)
-	string_to_print := ""
-	for thread, counters := range requestCounters {
-		for requestName, count := range counters {
-			if count > 0 {
-				if _, ok := func_counters[requestName]; !ok {
-					func_counters[requestName] = 0
-				}
-				func_counters[requestName] += count
-				string_to_print += fmt.Sprintf("	[%d] %s: %d\n", thread, requestName, count)
-				requestCounters[thread][requestName] = 0
-			}
+// func printCounters() {
+// 	time_now := time.Now()
+// 	func_counters := make(map[string]int)
+// 	string_to_print := ""
+// 	for thread, counters := range requestCounters {
+// 		for requestName, count := range counters {
+// 			if count > 0 {
+// 				if _, ok := func_counters[requestName]; !ok {
+// 					func_counters[requestName] = 0
+// 				}
+// 				func_counters[requestName] += count
+// 				string_to_print += fmt.Sprintf("	[%d] %s: %d\n", thread, requestName, count)
+// 				requestCounters[thread][requestName] = 0
+// 			}
+// 		}
+// 	}
+// 	if string_to_print != "" {
+// 		fmt.Printf("[%s] Slowpoke Counters\n", time_now.String())
+// 		fmt.Printf(string_to_print)
+// 		fmt.Printf("	[Aggregation]\n")
+// 		for func_name, count := range func_counters {
+// 			fmt.Printf("		%s: %d\n", func_name, count)
+// 		}
+// 	}
+// }
+
+func printCountersSyncMap() {
+	timeNow := time.Now()
+	funcCounters := make(map[string]int)
+	totalCounter := 0
+
+	// Iterate over sync.Map safely
+	requestCounters.Range(func(funcName, counter interface{}) bool {
+		funcNameStr, ok1 := funcName.(string)
+		counterInt, ok2 := counter.(int)
+
+		if !ok1 || !ok2 {
+			fmt.Println("Invalid type in sync.Map") // Prevent panic
+			return true
 		}
+
+		// Ensure no overwrites: Reset counter **before** adding it to funcCounters
+		for !requestCounters.CompareAndSwap(funcNameStr, counterInt, 0) {
+			// Reload latest value to prevent overwriting concurrent increments
+			newCounter, _ := requestCounters.Load(funcNameStr)
+			counterInt, _ = newCounter.(int)
+		}
+
+		// Now increment local counters after resetting the global counter
+		if counterInt > 0 {
+			funcCounters[funcNameStr] += counterInt
+			totalCounter += counterInt
+		}
+
+		return true
+	})
+
+	// Print results
+	msg := ""
+	msg += fmt.Sprintf("[%s] Slowpoke Counters\n", timeNow.Format(time.RFC3339))
+	for funcName, count := range funcCounters {
+		msg += fmt.Sprintf("\t%s: %d\n", funcName, count)
 	}
-	if string_to_print != "" {
-		fmt.Printf("[%s] Slowpoke Counters\n", time_now.String())
-		fmt.Printf(string_to_print)
-		fmt.Printf("	[Aggregation]\n")
-		for func_name, count := range func_counters {
-			fmt.Printf("		%s: %d\n", func_name, count)
-		}
+	msg += fmt.Sprintf("\t[Total] %d\n", totalCounter)
+	if totalCounter > 0 {
+		fmt.Println(msg)
 	}
 }
-
 
 // Saves the response to *res (also might save the result to cache if we are in upperbound baseline
 func performRequest[T interface{}](ctx context.Context, req *http.Request, res *T, app string, method string, argBytes []byte) {
@@ -65,13 +110,27 @@ func SlowpokeInit() {
 		fmt.Sscanf(env, "%d", &delayMicros)
 		fmt.Printf("SLOWPOKE_DELAY_MICROS=%d\n", delayMicros)
 	}
-	requestCounters = make(map[int]map[string]int)
-	// go func() {
-	// 	for {
-	// 		<-time.After(printIntervalMillis * time.Millisecond)
-	// 		printCounters()
-	// 	}
-	// }()
+	prerun = false
+	if env, ok := os.LookupEnv("SLOWPOKE_PRERUN"); ok {
+		if env == "true" {
+			prerun = true
+		}
+		fmt.Printf("SLOWPOKE_PRERUN=%t\n", prerun)
+	}
+
+	if !prerun {
+		return
+	}
+
+	// requestCounters = make(map[int]map[string]int)
+	requestCounters = sync.Map{}
+	go func() {
+		for {
+			<-time.After(printIntervalMillis * time.Millisecond)
+			// printCounters()
+			printCountersSyncMap()
+		}
+	}()
 }
 
 // Get the amount of time in nanoseconds the calling thread has spent using the CPU since startup
@@ -91,6 +150,22 @@ func SlowpokeCheck(serviceFuncName string) {
 	// 	requestCounters[unix.Gettid()][serviceFuncName] = 0
 	// }
 	// requestCounters[unix.Gettid()][serviceFuncName]++
+
+	if prerun{
+		// Record request
+		counter, _ := requestCounters.LoadOrStore(serviceFuncName, 0)
+
+		for {
+			current := counter.(int) // Safely type assert
+			if requestCounters.CompareAndSwap(serviceFuncName, current, current+1) {
+				break // Exit loop when successful
+			}
+		
+			// Retry in case of failure (another goroutine modified the value)
+			counter, _ = requestCounters.Load(serviceFuncName)
+		}
+	}
+	
 	// Delay
 	lockThread := true
 	if delayMicros >= 0 {
