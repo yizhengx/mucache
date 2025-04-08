e2286e8d6c650ddbac473c83ee93b6a02fe4a71a
Ubuntu <ubuntu@ip-172-31-12-246.ec2.internal>
2025-04-08 19:20:08 +0000

    Add chain-d8

--- slowpoke/synthetic/chain-d8-http-sync/run.sh
+++ slowpoke/synthetic/chain-d8-http-sync/run.sh
@@ -3,16 +3,16 @@
 cd $(dirname $0)/../..
 
 EXP=chain-d8-http-sync
-DIR=synthetic/$EXP/all-flush-fixed-json-time-based-sleep
+DIR=synthetic/$EXP/04-08-all-flush-fixed-json-time-based-sleep
 mkdir -p $DIR
 
 # config
 THREAD=8
 CONN=512
 NUM_REQ=20000
-POKER_BATCH=30000000
+POKER_BATCH_REQ=100
 NUM_EXP=10
-REPETITION=1
+REPETITION=2
 
 # Make it reproducible
 # target_service_random_pairs="0:14443 9:21601"
@@ -23,7 +23,7 @@ do
     target_service=$(echo $pair | cut -d':' -f1)
     random_seed=$(echo $pair | cut -d':' -f2)
 
-    output_file=$DIR/$EXP-service$target_service-t$THREAD-c$CONN-req$NUM_REQ-poker$POKER_BATCH-n$NUM_EXP-rep$REPETITION-larger-target-execT.log
+    output_file=$DIR/$EXP-service$target_service-t$THREAD-c$CONN-req$NUM_REQ-poker_batch_req$POKER_BATCH_REQ-n$NUM_EXP-rep$REPETITION.log
     
     if [[ -e $output_file ]]; then
         echo "File $output_file already exists. Skipping..."
@@ -41,6 +41,6 @@ do
         --num_req $NUM_REQ \
         --random_seed $random_seed \
         --repetition $REPETITION \
-        --poker_batch $POKER_BATCH \
+        --poker_batch_req $POKER_BATCH_REQ \
         >$output_file
 done
