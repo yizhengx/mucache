be59505f8e6695dfd3ff146e0ea2ff5c7a605142
Ubuntu <ubuntu@ip-172-31-36-226.ec2.internal>
2025-04-25 00:55:07 +0000

    Add res

--- slowpoke/synthetic/fanout-w3-grpc-async/run.sh
+++ slowpoke/synthetic/fanout-w3-grpc-async/run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Change this!!
-target_service_random_pairs="0:21032 2:18674" # Make it reproducible
+target_service_random_pairs="0:21032" # Make it reproducible 0:21032 2:18674
 
 cd $(dirname $0)/../..
 
 EXP=$(dirname $0 | xargs basename)
-DIR=synthetic/$EXP/04-09-pokerpp-rm-deadlock-0maxconn
+DIR=synthetic/$EXP/04-24-pokerpp-lowerconn
 mkdir -p $DIR
 
 # config
 THREAD=8
-CONN=800
+CONN=512
 NUM_REQ=20000
 POKER_BATCH_REQ=100
 NUM_EXP=10
-REPETITION=2
+REPETITION=5
 
 for pair in $target_service_random_pairs
 do 
